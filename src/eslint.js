fc7d3e878b0e3ee4c45a26f4aa80738640fa4aae
Kevin Ross <kevin.ross@alienfast.com>
2016-01-29 16:04:22 -0600

    eliminating jscs from the dependencies has things working again.

--- src/eslint.js
+++ src/eslint.js
@@ -1,12 +1,26 @@
 import BaseRecipe from './baseRecipe'
 import eslint from 'gulp-eslint'
 import extend from 'extend'
-import jscs from 'gulp-jscs'
-import stylish from 'gulp-jscs-stylish'
+//import jscs from 'gulp-jscs'
+//import stylish from 'gulp-jscs-stylish'
+import debug from 'gulp-debug'
 
 export const Default = {
+  debug: true,
   task: {
-    name: 'eslint'
+    name: 'esLint'
+  },
+  watch: {
+    glob: '**/*.js',
+    options: {
+      cwd: 'app/assets/javascripts'
+    }
+  },
+  source: {
+    glob: '**/*.js',
+    options: {
+      cwd: 'app/assets/javascripts'
+    }
   },
   options: {}
 }
@@ -28,17 +42,24 @@ const EsLint = class extends BaseRecipe {
   }
 
   run() {
-    return this.gulp.src(this.config.source)
-      // eslint() attaches the lint output to the "eslint" property of the file object so it can be used by other modules.
+    let bundle = this.gulp.src(this.config.source.glob, this.config.source.options)
+
+    if (this.config.debug) {
+      bundle.pipe(debug())
+    }
+
+    // eslint() attaches the lint output to the "eslint" property of the file object so it can be used by other modules.
+    bundle
       .pipe(eslint(this.config.options))
-      // eslint.format() outputs the lint results to the console. Alternatively use eslint.formatEach() (see Docs).
-      .pipe(eslint.format())
-      // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
-      .pipe(eslint.failAfterError())
-      .pipe(jscs())      // enforce style guide
-      .pipe(stylish())  // log style errors
-      //.pipe(jscs.reporter())
-      .pipe(jscs.reporter('fail')) // fail on error
+      .pipe(eslint.format()) // outputs the lint results to the console. Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.failAfterError()) // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
+
+    // FIXME: even including any remnant of JSCS at this point broke everything through the unfound requirement of babel 5.x through babel-jscs.  I can't tell where this occurred, but omitting gulp-jscs for now gets me past this issue.  Revisit this when there are clear updates to use babel 6
+    //.pipe(jscs())      // enforce style guide
+    //.pipe(stylish())  // log style errors
+    //.pipe(jscs.reporter('fail')) // fail on error
+
+    return bundle
   }
 
   // ----------------------------------------------

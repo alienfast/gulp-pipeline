2ccbedab4988de7071c90a73d6c18da4bcd9cd6d
Kevin Ross <kevin.ross@alienfast.com>
2016-02-03 14:48:12 -0600

    force eslint to notify on error

--- src/eslint.js
+++ src/eslint.js
@@ -5,6 +5,9 @@ import extend from 'extend'
 //import stylish from 'gulp-jscs-stylish'
 import debug from 'gulp-debug'
 import gulpif from 'gulp-if'
+import Util from 'gulp-util'
+
+let PluginError = Util.PluginError
 
 export const Default = {
   debug: false,
@@ -45,7 +48,27 @@ const EsLint = class extends BaseRecipe {
       .pipe(gulpif(this.config.debug, debug(this.debugOptions())))
       .pipe(eslint(this.config.options))
       .pipe(eslint.format()) // outputs the lint results to the console. Alternatively use eslint.formatEach() (see Docs).
+
+      // this should throw the error, but we aren't notified
       .pipe(gulpif(!watching, eslint.failAfterError())) // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
+
+      // make sure we are notified of any error (this really should be happening in eslint.failAfterError(), but not sure where it is lost)
+      .pipe(eslint.result((results) => {
+        let count = results.errorCount;
+        if (count > 0) {
+          throw new PluginError(
+            'gulp-eslint',
+            {
+              name: 'ESLintError',
+              message: 'Failed with ' + count + (count === 1 ? ' error' : ' errors')
+            }
+          )
+        }
+      }))
+      .on('error', (error) => {
+        this.notifyError(error, watching)
+      })
+
 
     // FIXME: even including any remnant of JSCS at this point broke everything through the unfound requirement of babel 5.x through babel-jscs.  I can't tell where this occurred, but omitting gulp-jscs for now gets me past this issue.  Revisit this when there are clear updates to use babel 6
     //.pipe(jscs())      // enforce style guide

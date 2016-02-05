e4b8fe82c0ca460c0328fb90c2b51f3373bcce9f
Kevin Ross <kevin.ross@alienfast.com>
2016-02-05 16:30:45 -0600

    committing all attempts at resolving https://github.com/adametry/gulp-eslint/issues/135#issuecomment-180595782

--- src/eslint.js
+++ src/eslint.js
@@ -49,11 +49,10 @@ const EsLint = class extends BaseRecipe {
       .pipe(eslint(this.config.options))
       .pipe(eslint.format()) // outputs the lint results to the console. Alternatively use eslint.formatEach() (see Docs).
 
+
+      //1. HACK solution that works with first error, but is very ugly
       // this should emit the error, but we aren't notified
       .pipe(gulpif(!watching, eslint.failAfterError())) // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
-
-
-      // 1. HACK solution that works with first error, but is very ugly
 
       // make sure we are notified of any error (this really should be happening in eslint.failAfterError(), but not sure where it is lost)
       .pipe(eslint.result((results) => { // this is single file #result not #results, we don't get notified on #results
@@ -70,6 +69,83 @@ const EsLint = class extends BaseRecipe {
       .on('error', (error) => {
         this.notifyError(error, watching)
       })
+
+      // 2. Attempt now that returns are in place with the gulpif
+      // this should emit the error, but we aren't notified
+      //.pipe(gulpif(!watching, eslint.failAfterError())) // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
+      //.on('error', (error) => {
+      //  this.notifyError(error, watching)
+      //})
+
+      //// 3. Attempt now that returns are in place WITHOUT gulpif
+      //// this should emit the error, but we aren't notified
+      //.pipe( eslint.failAfterError()) // To have the process exit with an error code (1) on lint error, return the stream and pipe to failAfterError last.
+      //.on('error', (error) => {
+      //  this.notifyError(error, watching)
+      //})
+
+      // 4. https://github.com/adametry/gulp-eslint/issues/135#issuecomment-180555978
+      //.pipe(eslint.results(function (results) {
+      //  var count = results.errorCount;
+      //  console.log('Total ESLint Error Count: ' + count);
+      //  if (count > 0) {
+      //    throw new Error('Failed with Errors');
+      //  }
+      //}))
+      //.on('error', function (error) {
+      //  console.log('Total ESLint Error Count: ' + error);
+      //})
+      //.on('finish', function () {
+      //  console.log('eslint.results finished');
+      //})
+      //.on('end', function () {
+      //  console.log('eslint.results ended');
+      //})
+
+      //// 5. notification is emitted
+      //.pipe(eslint.results(function (results) {
+      //  var count = results.errorCount;
+      //  console.log('*****Error Count: ' + count);
+      //  if (count > 0) {
+      //    throw new Error('******My custom error');
+      //  }
+      //}))
+      //.on('error', (error) => {
+      //  this.notifyError(error, watching)
+      //})
+
+
+      //// 6. notification is emitted
+      //.pipe(eslint.results(function (results) {
+      //  var count = results.errorCount;
+      //  console.log('*****Error Count: ' + count);
+      //  if (count > 0) {
+      //    throw new PluginError('******My custom error');
+      //  }
+      //}))
+      //.on('error', (error) => {
+      //  this.notifyError(error, watching)
+      //})
+
+      //// 7. notification is emitted, except when watching
+      //.pipe(eslint.results(function (results) {
+      //  let count = results.errorCount;
+      //  console.error('****************in results handler')
+      //  if (count > 0) {
+      //    throw new PluginError('gulp-eslint', { message: 'Failed with ' + count + (count === 1 ? ' error' : ' errors') })
+      //  }
+      //}))
+      //.on('error', (error) => {
+      //  console.error('****************in error handler')
+      //  this.notifyError(error, watching)
+      //})
+
+
+      //.pipe( eslint.failAfterError())
+      //.on('error', (error) => {
+      //  this.notifyError(error, watching)
+      //})
+
 
 
     // FIXME: even including any remnant of JSCS at this point broke everything through the unfound requirement of babel 5.x through babel-jscs.  I can't tell where this occurred, but omitting gulp-jscs for now gets me past this issue.  Revisit this when there are clear updates to use babel 6

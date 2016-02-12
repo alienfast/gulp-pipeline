3721818460c81d01d8a5ce6d4e17c8778acdba01
Kevin Ross <kevin.ross@alienfast.com>
2016-02-12 17:47:31 -0600

    Added cache for baseDirectories in the rails environment

--- src/rails.js
+++ src/rails.js
@@ -1,29 +1,67 @@
 import path from 'path'
 import spawn from 'cross-spawn'
+import fs from 'fs'
+import jsonfile from 'jsonfile'
+import Util from 'gulp-util'
+
 //import stringify from 'stringify-object'
 
-const Rails = class {
-  static filePath(name) {
-    return path.join(__dirname, `rails/${name}`) // eslint-disable-line no-undef
-  }
+const BaseDirectoriesCache = `.gulp-pipeline-rails.json`
+const GemfileLock = `Gemfile.lock`
 
+const Rails = class {
   static enumerateEngines() {
     let results = spawn.sync(this.filePath('railsRunner.sh'), [this.filePath('enumerateEngines.rb')], {sdtio: 'inherit'})
     return JSON.parse(results.output[1])
   }
 
+  static filePath(name) {
+    return path.join(__dirname, `rails/${name}`) // eslint-disable-line no-undef
+  }
+
   static baseDirectories() {
-    // FIXME:  Rails engines only need to be resolved when the gemfile.lock has changed, need to cache this list to speed up subsequent runs
-    let engines = Rails.enumerateEngines()
-    //console.log(stringify(engines))
-
-    let baseDirectories = ['./']
-    for (let key of Object.keys(engines)) {
-      baseDirectories.push(engines[key])
+    if (!this.changed(GemfileLock, BaseDirectoriesCache)) {
+      return jsonfile.readFileSync(BaseDirectoriesCache)
     }
-    //console.log(stringify(baseDirectories))
+    else {
+      Util.log(`Generating baseDirectories cache...`)
+      try {
+        fs.unlinkSync(BaseDirectoriesCache)
+      } catch (error) {
+        //ignore
+      }
 
-    return {baseDirectories: baseDirectories}
+      let engines = Rails.enumerateEngines()
+      //console.log(stringify(engines))
+
+      let baseDirectories = ['./']
+      for (let key of Object.keys(engines)) {
+        baseDirectories.push(engines[key])
+      }
+      //console.log(stringify(baseDirectories))
+      let result = {baseDirectories: baseDirectories}
+      jsonfile.writeFileSync(BaseDirectoriesCache, result, {spaces: 2})
+      return result
+    }
+  }
+
+  static changed(sourceFileName, targetFileName) {
+    let sourceStat = null
+    let targetStat = null
+    try {
+      sourceStat = fs.statSync(sourceFileName)
+      targetStat = fs.statSync(targetFileName)
+    }
+    catch (error) {
+      return true
+    }
+
+    if (sourceStat.mtime > targetStat.mtime) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 }
 

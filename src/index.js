b182a96b95f7d61b4fe5f46e7dba95bd6a33a733
Kevin Ross <kevin.ross@alienfast.com>
2016-03-17 18:47:51 -0500

    change minifyCss to cssNano to better represent the underlying plugin, and make sure nano'd files in the same directory get the min.css extension

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export { default as CleanDigest } from './cleanDigest'
 export { default as Clean } from './clean'
 
 export { default as Rev } from './rev'
-export { default as MinifyCss } from './minifyCss'
+export { default as CssNano } from './cssNano'
 
 export { default as Mocha } from './mocha'
 

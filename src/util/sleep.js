8e994dec6e6bbf6df4b4693e1ce3e9bc64686342
Kevin Ross <kevin.ross@alienfast.com>
2016-03-22 21:18:06 -0500

    sleep is a one-off and has a ms parameter

--- src/util/sleep.js
+++ src/util/sleep.js
@@ -4,7 +4,7 @@ import Sleep from '../sleep'
  * Simplified sleep() that uses the Sleep recipe
  */
 const sleep = (gulp, ms) => {
-  let c = new Sleep(gulp, {}, {sleep: ms})
+  let c = new Sleep(gulp, {}, ms)
   // set the display name so it shows up in the task list
   c.taskFn.displayName = `<sleep>`
   return c

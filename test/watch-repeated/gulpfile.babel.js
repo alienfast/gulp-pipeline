f8e5d272b7a913e1195324ff85caec2af12af017
Kevin Ross <kevin.ross@alienfast.com>
2016-04-29 19:10:42 -0500

    try connecting to events

--- test/watch-repeated/gulpfile.babel.js
+++ test/watch-repeated/gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import Preset from '../../src/preset'
 import Sass from '../../src/sass'
 import gulpSass from 'gulp-sass'
+import debug from 'gulp-debug'
 
 const preset = Preset.nodeSrc()
 
@@ -9,15 +10,30 @@ new Sass(gulp, preset) // creates sass and sass:watch
 
 var t = function (done) {
   return gulp.src(['*.scss', '!_*.scss'], {cwd: 'scss'})
+    .pipe(debug())
     .pipe(gulpSass())
     .on('error', (error) => {
       done(error)
     })
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
 }
 
 gulp.task('simple:sass', t)
 
 gulp.task('simple:sass:watch', () => {
-  gulp.watch('**/*.scss', {cwd: 'scss'}, t);
+  let watcher = gulp.watch('**/*.scss', {cwd: 'scss'}, t)
+  watcher.on('error', (error) => {
+    console.log(`Error via watcher: ${error}`)
+  })
+
+  watcher.on('add', (path) => {
+    console.log(`${path} was added, running...`)
+  })
+
+  watcher.on('change', (path) => {
+    console.log(`${path} was changed, running...`)
+  })
+  watcher.on('unlink', (path) => {
+    console.log(`${path} was deleted, running...`)
+  })
 })
